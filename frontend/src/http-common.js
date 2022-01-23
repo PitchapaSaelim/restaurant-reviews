2db71db4a4cfffb1fde7740c19de73e6ebebb4a0
PitchapaSaelim <pitchapa.sae@ku.th>
2022-01-24 03:21:19 +0700

    feat: Setup MongoDB Realm and replace backend

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "http://localhost:5000/api/v1/restaurants",
+    "https://ap-southeast-1.aws.data.mongodb-api.com/app/restaurant-reviews-acfyd/endpoint/",
   headers: {
     "Content-type": "application/json",
   },
