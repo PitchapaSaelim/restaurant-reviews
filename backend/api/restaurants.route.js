08ec30b50542b03d3fc0ccc31cb91c9c84e3a3f2
PitchapaSaelim <pitchapa.sae@ku.th>
2022-01-23 15:59:46 +0700

    Can Add, Edit, and Delete Reviews

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,8 +1,15 @@
-import express from "express"
-import RestaurantsCtrl from "./restaurants.controller.js"
+import express from "express";
+import RestaurantsCtrl from "./restaurants.controller.js";
+import ReviewsCtrl from "./reviews.controller.js";
 
-const router = express.Router() // access express router
+const router = express.Router(); // access express router
 
-router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
 
-export default router
+router
+  .route("/review")
+  .post(ReviewsCtrl.apiPostReview)
+  .put(ReviewsCtrl.apiUpdateReview)
+  .delete(ReviewsCtrl.apiDeleteReview);
+
+export default router;
